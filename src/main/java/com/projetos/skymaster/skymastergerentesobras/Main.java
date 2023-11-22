180fc97c4dd9d89d174b651306dc7534fc17fce6
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-11-22 15:06:15 -0300

    Alterando ponto de entrada da aplicação

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/Main.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/TelaInicial.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/Login.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
 

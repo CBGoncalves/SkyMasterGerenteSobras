fb6aebf3dc90b9f8dea904a21fa6570563eaf560
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-11-28 15:42:46 -0300

    [SM-37] tela de listagem de tipo de item com estilizacao

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/tipoItem/ListarTipoItemController.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/tipoItem/ListarTipoItemController.java
@@ -1,10 +1,18 @@
 package com.projetos.skymaster.skymastergerentesobras.controllers.tipoItem;
 
 import com.projetos.skymaster.skymastergerentesobras.controllers.NavigationBarController;
+import com.projetos.skymaster.skymastergerentesobras.dao.MarcaDao;
+import com.projetos.skymaster.skymastergerentesobras.dao.TipoItemDao;
+import com.projetos.skymaster.skymastergerentesobras.models.Marca;
+import com.projetos.skymaster.skymastergerentesobras.models.TipoItem;
 import com.projetos.skymaster.skymastergerentesobras.models.TipoUsuarioNav;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -13,6 +21,18 @@ import java.sql.SQLException;
 public class ListarTipoItemController {
     @FXML
     private AnchorPane root;
+    @FXML
+    private TableView tableView;
+    @FXML
+    private TableColumn<TipoItem, Integer> codTipoItemColumn;
+    @FXML
+    private TableColumn<TipoItem, String> nomeTipoItemColumn;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private Button btnDeletar;
+
+    private TipoItemDao tipoItemDao;
 
     public void initialize() throws SQLException {
 
@@ -34,5 +54,13 @@ public class ListarTipoItemController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+
+    public void handleEditarButtonAction(ActionEvent event) {
+    }
+
+    public void handleDeletarButtonAction(ActionEvent event) {
     }
 }

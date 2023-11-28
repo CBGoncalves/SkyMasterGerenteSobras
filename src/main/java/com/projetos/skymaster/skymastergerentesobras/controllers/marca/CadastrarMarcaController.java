5729773f647842c496bb6f3806cce5c17e513b9c
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-11-28 07:23:06 -0300

    [SM-30] tela de cadastro com componentes e estilizacao/botao cancelar funcionando

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/marca/CadastrarMarcaController.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/marca/CadastrarMarcaController.java
@@ -2,10 +2,16 @@ package com.projetos.skymaster.skymastergerentesobras.controllers.marca;
 
 import com.projetos.skymaster.skymastergerentesobras.controllers.NavigationBarController;
 import com.projetos.skymaster.skymastergerentesobras.models.TipoUsuarioNav;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -13,6 +19,14 @@ import java.sql.SQLException;
 public class CadastrarMarcaController {
     @FXML
     private AnchorPane root;
+    @FXML
+    private TextField campoCodMarca;
+    @FXML
+    private TextField campoNomeMarca;
+    @FXML
+    private Button btnCadastrar;
+    @FXML
+    private Button btnCancelar;
 
     public void initialize() throws SQLException {
 
@@ -34,5 +48,25 @@ public class CadastrarMarcaController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void handleCadastrarButtonAction(ActionEvent event) {
+    }
+
+    public void handleCancelarButtonAction(ActionEvent event) throws IOException{
+        Stage stageCadastroMarca = (Stage) btnCancelar.getScene().getWindow();
+        stageCadastroMarca.close();
+
+        Image icon = new Image(getClass().getResourceAsStream("/com/projetos/skymaster/skymastergerentesobras/img/logo_sky_reduzida.jpg"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/projetos/skymaster/skymastergerentesobras/views/TelaInicial.fxml"));
+        Parent root = loader.load();
+
+        Stage telaInicial = new Stage();
+        telaInicial.setTitle("Tela Inicial");
+        telaInicial.setScene(new Scene(root));
+        telaInicial.setResizable(false);
+        telaInicial.getIcons().add(icon);
+        telaInicial.show();
     }
 }

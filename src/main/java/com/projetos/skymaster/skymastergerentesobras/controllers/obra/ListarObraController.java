e231c5f5adbb363d85d6532212a5f0cf7c3bd98e
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-11-28 13:41:02 -0300

    [SM-41] metodo de listagem de obras finalizado

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/obra/ListarObraController.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/obra/ListarObraController.java
@@ -1,7 +1,9 @@
 package com.projetos.skymaster.skymastergerentesobras.controllers.obra;
 
 import com.projetos.skymaster.skymastergerentesobras.controllers.NavigationBarController;
+import com.projetos.skymaster.skymastergerentesobras.dao.MarcaDao;
 import com.projetos.skymaster.skymastergerentesobras.dao.ObraDao;
+import com.projetos.skymaster.skymastergerentesobras.models.Marca;
 import com.projetos.skymaster.skymastergerentesobras.models.Obra;
 import com.projetos.skymaster.skymastergerentesobras.models.TipoUsuarioNav;
 import com.projetos.skymaster.skymastergerentesobras.models.Usuario;
@@ -12,10 +14,12 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class ListarObraController {
     @FXML
@@ -30,6 +34,7 @@ public class ListarObraController {
     private Button btnEditar;
     @FXML
     private Button btnDeletar;
+    private ObraDao obraDao;
 
     public void initialize() throws SQLException {
 
@@ -53,6 +58,18 @@ public class ListarObraController {
         }
 
         tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        obraDao = new ObraDao();
+
+        codObraColumn.setCellValueFactory(new PropertyValueFactory<>("codObra"));
+        nomeObraColumn.setCellValueFactory(new PropertyValueFactory<>("nomeObra"));
+
+        try {
+            List<Obra> obras = obraDao.selectAllObras();
+            tableView.getItems().addAll(obras);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void handleEditarButtonAction(ActionEvent event) {

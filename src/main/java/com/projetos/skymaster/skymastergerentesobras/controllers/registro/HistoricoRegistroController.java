ca3cf45b92bb1e5a50332339763b559404a92d19
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-12-01 10:59:27 -0300

    [SM-47] listagem de entradas e saidas por data

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/registro/HistoricoRegistroController.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/registro/HistoricoRegistroController.java
@@ -1,24 +1,70 @@
 package com.projetos.skymaster.skymastergerentesobras.controllers.registro;
 
 import com.projetos.skymaster.skymastergerentesobras.controllers.NavigationBarController;
+import com.projetos.skymaster.skymastergerentesobras.dao.RegistroDao;
+import com.projetos.skymaster.skymastergerentesobras.models.Registro;
 import com.projetos.skymaster.skymastergerentesobras.models.TipoUsuarioNav;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 
 public class HistoricoRegistroController {
     @FXML
     private AnchorPane root;
     @FXML
-    private TableView tableView;
+    private TableView<Registro> tableView;
+    @FXML
+    private TableColumn<Registro, String> tipoColumn;
+    @FXML
+    private TableColumn<Registro, String> numNotaColumn;
+    @FXML
+    private TableColumn<Registro, Integer> qtdColumn;
+    @FXML
+    private TableColumn<Registro, String> descricaoColumn;
+    @FXML
+    private TableColumn<Registro, String> tipoItemColumn;
+    @FXML
+    private TableColumn<Registro, String> marcaColumn;
+    @FXML
+    private TableColumn<Registro, String> obraColumn;
+    @FXML
+    private TableColumn<Registro, String> usuarioColumn;
+    @FXML
+    private TableColumn<Registro, LocalDate> dataColumn;
+
+    private RegistroDao registroDao;
 
     public void initialize() throws SQLException {
+
+        registroDao = new RegistroDao();
+
+        tipoColumn.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+        numNotaColumn.setCellValueFactory(new PropertyValueFactory<>("numNotaEntrada"));
+        qtdColumn.setCellValueFactory(new PropertyValueFactory<>("quantidade"));
+        descricaoColumn.setCellValueFactory(new PropertyValueFactory<>("descricaoItem"));
+        tipoItemColumn.setCellValueFactory(new PropertyValueFactory<>("nomeTipoItem"));
+        marcaColumn.setCellValueFactory(new PropertyValueFactory<>("nomeMarca"));
+        obraColumn.setCellValueFactory(new PropertyValueFactory<>("nomeObra"));
+        usuarioColumn.setCellValueFactory(new PropertyValueFactory<>("nomeUsuario"));
+        dataColumn.setCellValueFactory(new PropertyValueFactory<>("data"));
+
+        try {
+            List<Registro> registros = registroDao.selectRegistersByDate();
+
+            tableView.getItems().addAll(registros);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/projetos/skymaster/skymastergerentesobras/views/NavigationBar.fxml"));

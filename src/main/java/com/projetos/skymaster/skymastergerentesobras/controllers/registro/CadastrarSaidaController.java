947553baff052e00c3554b358a9a4ff6372db5d6
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-12-01 09:36:41 -0300

    [SM-46] componentes e estilizacao da tela

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/registro/CadastrarSaidaController.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/registro/CadastrarSaidaController.java
@@ -2,6 +2,7 @@ package com.projetos.skymaster.skymastergerentesobras.controllers.registro;
 
 import com.projetos.skymaster.skymastergerentesobras.controllers.NavigationBarController;
 import com.projetos.skymaster.skymastergerentesobras.models.TipoUsuarioNav;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -34,5 +35,11 @@ public class CadastrarSaidaController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void handleRegistrarButtonAction(ActionEvent event) {
+    }
+
+    public void handleCancelarButtonAction(ActionEvent event) {
     }
 }

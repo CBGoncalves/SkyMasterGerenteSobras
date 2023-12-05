971ed57250f42e325ea594321731e1c7de2774c6
CBGoncalves <carlos.eduardo46851@gmail.com>
2023-12-05 14:37:51 -0300

    [SM-50] metodo botao cancelar funcionando

--- src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/GerarRelatorioController.java
+++ src/main/java/com/projetos/skymaster/skymastergerentesobras/controllers/GerarRelatorioController.java
@@ -1,10 +1,15 @@
 package com.projetos.skymaster.skymastergerentesobras.controllers;
 
 import com.projetos.skymaster.skymastergerentesobras.models.TipoUsuarioNav;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -12,6 +17,8 @@ import java.sql.SQLException;
 public class GerarRelatorioController {
     @FXML
     private AnchorPane root;
+    @FXML
+    private Button btnCancelar;
 
     public void initialize() throws SQLException {
 
@@ -33,5 +40,25 @@ public class GerarRelatorioController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void handleGerarButtonAction(ActionEvent event) {
+    }
+
+    public void handleCancelarButtonAction(ActionEvent event) throws IOException{
+        Stage stageGerarRelatorio = (Stage) btnCancelar.getScene().getWindow();
+        stageGerarRelatorio.close();
+
+        Image icon = new Image(getClass().getResourceAsStream("/com/projetos/skymaster/skymastergerentesobras/img/logo_sky_reduzida.jpg"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/projetos/skymaster/skymastergerentesobras/views/TelaInicial.fxml"));
+        Parent root = loader.load();
+
+        Stage telaInicial = new Stage();
+        telaInicial.setTitle("Tela Inicial");
+        telaInicial.setScene(new Scene(root));
+        telaInicial.setResizable(false);
+        telaInicial.getIcons().add(icon);
+        telaInicial.show();
     }
 }
